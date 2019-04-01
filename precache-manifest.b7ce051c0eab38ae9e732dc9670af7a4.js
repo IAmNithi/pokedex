e90583b98df1275fbdc2496558a3ff81c321f73f
IAmNithi <kalanithi_sas@outlook.com>
2019-04-01 15:45:04 +0530

    Updates

---
+++ precache-manifest.b7ce051c0eab38ae9e732dc9670af7a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  },
+  {
+    "revision": "36c65d8e24858a0ffc7a",
+    "url": "/pokedex/static/js/main.b323ce88.chunk.js"
+  },
+  {
+    "revision": "4e68fd58a52ec4b6266e",
+    "url": "/pokedex/static/js/2.b6444f1d.chunk.js"
+  },
+  {
+    "revision": "36c65d8e24858a0ffc7a",
+    "url": "/pokedex/static/css/main.ac04d171.chunk.css"
+  },
+  {
+    "revision": "4e68fd58a52ec4b6266e",
+    "url": "/pokedex/static/css/2.47805710.chunk.css"
+  },
+  {
+    "revision": "7d34fe3aea2c3ccccfa1b66f2d018e0c",
+    "url": "/pokedex/index.html"
+  }
+];
